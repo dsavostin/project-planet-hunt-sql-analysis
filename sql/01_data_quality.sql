a0d0546ebf3fcb038de5b40d787d35791bff5685
Dmitriy Savostin <juster09@gmail.com>
2026-07-21 09:56:03 +0300

    Add files via upload

---
+++ sql/01_data_quality.sql
@@ -0,0 +1,17 @@
+-- Проверка уникальности идентификаторов игроков
+
+-- Общее количество строк
+SELECT COUNT(*)
+FROM players_profile;
+
+-- Количество непустых player_id
+SELECT COUNT(player_id)
+FROM players_profile;
+
+-- Количество уникальных player_id
+SELECT COUNT(DISTINCT player_id)
+FROM players_profile;
+
+-- Вывод:
+-- Таблица players_profile содержит 3078 записей.
+-- NULL и дубликаты по player_id отсутствуют.
