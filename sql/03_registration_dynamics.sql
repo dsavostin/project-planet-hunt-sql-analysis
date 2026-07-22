0817ae396bc2c29fe507a584b7b15044996c9e11
Dmitriy Savostin <juster09@gmail.com>
2026-07-22 18:49:10 +0300

    Add files via upload

--- sql/03_registration_dynamics.sql
+++ sql/03_registration_dynamics.sql
@@ -6,8 +6,8 @@
 и найти месяцы с максимальным приростом новых пользователей.
 */
 
-SELECT DATE_TRUNC('month', registration_date)::date AS reg_month,
-       COUNT(*) AS new_users
+SELECT DATE_TRUNC('month', registration_date)::date AS reg_month
+     , COUNT(*) AS new_users
 FROM planet_hunt.players_profile
 GROUP BY reg_month
 ORDER BY reg_month
